0f39121b2aa0ef13db81024a1389d962c3c12f14
duanaihua <duanaihua@gintong.com>
2017-09-05 10:32:45 +0800

    add sharetype

--- phoenix-track-entity/src/main/java/com/ginkgocap/ywxt/track/entity/constant/ShareTypeEnum.java
+++ phoenix-track-entity/src/main/java/com/ginkgocap/ywxt/track/entity/constant/ShareTypeEnum.java
@@ -5,15 +5,15 @@ package com.ginkgocap.ywxt.track.entity.constant;
  */
 public enum ShareTypeEnum {
 
-    SHARE_DYNAMIC(1,"动态"),
-    SHARE_CHET(2,"畅聊"),
-    SHARE_ASSOCIATION(3,"社群"),
-    SHARE_AFFAIR(4,"事务"),
-    SHARE_FRIEND_CIRCLE(4,"朋友圈"),
-    SHARE_QQ(4,"QQ"),
-    SHARE_WECHAT(4,"微信"),
-    SHARE_MICROBLOG(4,"微博"),
-    SHARE_OTHER(5,"其它方式");
+    SHARE_DYNAMIC(11,"动态"),
+    SHARE_CHET(12,"畅聊"),
+    SHARE_ASSOCIATION(13,"社群"),
+    SHARE_AFFAIR(14,"事务"),
+    SHARE_FRIEND_CIRCLE(21,"朋友圈"),
+    SHARE_QQ(22,"QQ"),
+    SHARE_WECHAT(23,"微信"),
+    SHARE_MICROBLOG(24,"微博"),
+    SHARE_OTHER(25,"其它方式");
 
     private int key;
     private String value;
